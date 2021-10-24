9a087485092e38740dbbc15614b91a84ae1d461c
GabiRP <ruizperezgabriel@hotmail.com>
2021-10-24 17:54:37 +0200

    Yes

--- CoinTweaks/EventHandlers.cs
+++ CoinTweaks/EventHandlers.cs
@@ -13,15 +13,13 @@ namespace CoinTweaks
 
         internal void OnFlippingCoin(FlippingCoinEventArgs ev)
         {
-            if (plugin.Config.DropCoinChance != 0 && UnityEngine.Random.Range(1, 101) <= plugin.Config.DropCoinChance)
+            if (plugin.Config.DropCoinChance > 0 && UnityEngine.Random.Range(1, 100) <= plugin.Config.DropCoinChance)
             {
-                var coin = ev.Player.Items.First(x => x.Type == ItemType.Coin);
                 Timing.CallDelayed(plugin.Config.DropCoinTime, () =>
                 {
-                    ev.Player.DropItem(coin);
+                    ev.Player.DropItem(ev.Player.CurrentItem);
                     if (plugin.Config.UseHints) 
                         ev.Player.ShowHint(plugin.Translation.DropCoinMessage, plugin.Config.DropCoinMessageDuration);
-                    
                     else
                         ev.Player.Broadcast(plugin.Config.DropCoinMessageDuration, plugin.Translation.DropCoinMessage, Broadcast.BroadcastFlags.Normal, true);
                 });
@@ -31,10 +29,10 @@ namespace CoinTweaks
             {
                 Timing.CallDelayed(plugin.Config.CoinResultTime, () =>
                 {
-                    if (!plugin.Config.UseHints)
-                        ev.Player.Broadcast(plugin.Config.CoinResultMessageDuration, plugin.Translation.CoinResultMessage.Replace("%result%", ev.IsTails ? plugin.Translation.TailsTranslation : plugin.Translation.HeadsTranslation), Broadcast.BroadcastFlags.Normal, true);
-                    else
+                    if (plugin.Config.UseHints)
                         ev.Player.ShowHint(plugin.Translation.CoinResultMessage.Replace("%result%", ev.IsTails ? plugin.Translation.TailsTranslation : plugin.Translation.HeadsTranslation), plugin.Config.CoinResultMessageDuration);
+                    else
+                        ev.Player.Broadcast(plugin.Config.CoinResultMessageDuration, plugin.Translation.CoinResultMessage.Replace("%result%", ev.IsTails ? plugin.Translation.TailsTranslation : plugin.Translation.HeadsTranslation));
                 });
             }
         }
